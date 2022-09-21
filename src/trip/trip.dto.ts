97a42ca9dcab364c8b0ee996592ab23f0e39c1fd
krutulkamil <krutulkamil@onet.pl>
2022-09-21 21:37:26 +0200

    feat: trip dto

---
+++ src/trip/trip.dto.ts
@@ -0,0 +1,29 @@
+import { IsString, IsNumber, IsDate, MinDate, MaxDate, IsPositive, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+
+const today = new Date();
+const yesterday = new Date(today);
+yesterday.setDate(yesterday.getDate() - 1);
+const maxDate = new Date(2023, 0, 1)
+
+export class TripDto {
+    @IsString()
+    @IsNotEmpty({ message: 'Start address is required!' })
+    startAddress: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Destination address is required!' })
+    destinationAddress: string;
+
+    @IsNumber({}, { message: 'Price must be a number!' })
+    @IsNotEmpty({ message: 'Price is required!' })
+    @IsPositive({ message: 'Price must be a positive number!' })
+    price: number;
+
+    @IsDate({ message: 'Date must be in proper date format!' })
+    @Type(() => Date)
+    @IsNotEmpty({ message: 'Date of a delivery is required!' })
+    @MinDate(yesterday, { message: 'Cannot add date before today!' })
+    @MaxDate(maxDate, { message: 'Only dates before 2023 supported!' })
+    date: Date;
+}
