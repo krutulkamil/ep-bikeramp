c35429b44aa5fef9df321abea22fe5f4b32d21ca
krutulkamil <krutulkamil@onet.pl>
2022-09-21 20:21:48 +0200

    feat: config module & typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TripModule } from './trip/trip.module';
 
 @Module({
-    imports: [TripModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [configuration]
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: true
+        }),
+        TripModule
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api");
-    await app.listen(3000);
+
+    const port = +process.env.PORT || 4000;
+    await app.listen(port);
 }
 
 bootstrap();
